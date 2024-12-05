1f3e2aa436fe06abdec124a3dc2d6f2f8eb92822
devyej8753 <devyej8753@gmail.com>
2024-12-05 14:00:19 +0900

    [lyj] pcce s2

--- pcce_intro/src/day01/Run.java
+++ pcce_intro/src/day01/Run.java
@@ -10,6 +10,11 @@ public class Run {
 		int n2 = s1.solution(100, 2);
 		System.out.println("100, 2일때 : "+n2);
 		
+		Solution02 s2 = new Solution02();
+		int n3 = s2.solution(2, 3);
+		System.out.println("2,3일 때 : "+n3);
+		int n4 = s2.solution(100, 2);
+		System.out.println("100,2일 때 : "+n4);
 		
 		
 		
