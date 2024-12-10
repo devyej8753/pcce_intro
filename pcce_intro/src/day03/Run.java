3e4ef5bc5dc69051a0940e3e71f3ec6fed8fd943
devyej8753 <devyej8753@gmail.com>
2024-12-10 12:24:21 +0900

    [lyj] pcce

---
+++ pcce_intro/src/day03/Run.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Run {
+	public static void main(String[] args) {
+		Solution01 s = new Solution01();
+		int su = s.solution(3, 2);
+		System.out.println("3, 2일 때 : "+ su);
+		int su1 = s.solution(10, 5);
+		System.out.println("10, 5일 때 : "+ su);
+		
+	}
+	
+
+	
+}
+
+
---
+++ pcce_intro/src/day03/Solution01.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class Solution01 {
+	
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1%num2;
+        return answer;
+    }
+}
